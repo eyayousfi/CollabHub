18fa336e31a4fd6037e85484b8b40053b83dd6e4
Eyaaaa <eya.yousfi@esprit.tn>
2025-02-16 10:01:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useMemo, useState } from 'react';
+import { createTheme, Button, ThemeProvider, CssBaseline } from '@mui/material';
+import getDesignTokens from 'styles/MyTheme';
+
+
+
+function App() {
+  const [mode, setmyMOde] = useState(
+    localStorage.getItem("currentMode") === null
+      ? "dark"
+      : localStorage.getItem("currentMode") === "light"
+      ? "light"
+      : "dark"
+  );
+
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+  return (
+
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    <div className="App">
+
+
+      <Button onClick={() => {
+              localStorage.setItem(
+                "currentMode",
+                theme.palette.mode === "dark" ? "light" : "dark"
+              );
+
+              setmyMOde(theme.palette.mode === "light" ? "dark" : "light");
+            }} variant="text" color="primary">
+        change mode 
+      </Button>
+      
+    </div>
+
+    </ThemeProvider>
+  );
+}
+
+export default App;
